3fc617217d274bee6cb49e90e2496cd0def9d975
irakli2896 <90774410+irakli2896@users.noreply.github.com>
2022-06-12 01:03:11 +0400

    first lecture

---
+++ my-app/src/Greeting.js
@@ -0,0 +1,8 @@
+const Greeting = ({title, subtitle}) => {
+    return <header>
+      <h1>{title}</h1>
+      <h4>{subtitle}</h4>
+    </header>  
+  }
+
+  export default Greeting;
---
+++ my-app/src/Welcome.js
@@ -0,0 +1,10 @@
+const Welcome = ({user, age}) => {
+    return (
+    <div>
+        <h3>Welcome, {user}. </h3>
+        <h2>Your age is {age}. </h2>
+    </div>
+    )
+};
+
+export default Welcome;
